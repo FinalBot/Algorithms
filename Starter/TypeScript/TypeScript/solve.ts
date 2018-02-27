c7b421a5c0150151727dd87edace9c2adc0f30d1
Ed Kaim <ed@sharplogic.com>
2018-02-27 12:59:07 -0800

    Initial TypeScript starter kit

---
+++ Starter/TypeScript/TypeScript/solve.ts
@@ -0,0 +1,11 @@
+﻿class Solver {
+    solve(input: string): string {
+        var parts = input.split(' ');
+        return (parseInt(parts[0]) + parseInt(parts[1])).toString();
+    }
+}
+
+function solve(input: string): string {
+    var solver: Solver = new Solver();
+    return solver.solve(input);
+}
