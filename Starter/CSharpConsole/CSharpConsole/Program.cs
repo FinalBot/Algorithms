08ef14cc6fd78d00127bb64db62c8d5db89d98b9
Ed Kaim <ed@sharplogic.com>
2018-02-27 13:23:32 -0800

    Initial C# console starter

---
+++ Starter/CSharpConsole/CSharpConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A simple console test.
+            Console.WriteLine(Solve("1 2"));
+        }
+
+        /// <summary>
+        /// This is the entry point to your solution. You can add whatever code and classes you need (within the
+        /// security restrictions), but just be sure this method exists to accept and return a string.
+        /// </summary>
+        /// <param name="input">The input for the test case.</param>
+        /// <returns>The output to test.</returns>
+        static public string Solve(string input)
+        {
+            string[] parts = input.Split(' ');
+            return (int.Parse(parts[0]) + int.Parse(parts[1])).ToString();
+        }
+    }
+}
---
+++ Starter/CSharpConsole/readme.md
@@ -0,0 +1,6 @@
+# C# Console Starter
+This solution contains one key file:
+
+1. **Program.cs** contains a single static method called **Solve**. This is the entry point to your solution.
+
+The target file to submit as your solution is **bin\Debug\CSharpConsole.exe**.
